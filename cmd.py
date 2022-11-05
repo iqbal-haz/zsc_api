25f975929374813bc8148cd808e58c90184d0c05
AI Engineer <iqbal@Bahasas-MBP-2.mshome.net>
2022-11-05 21:37:35 +0700

    cmd script thru python

---
+++ cmd.py
@@ -0,0 +1,10 @@
+import os
+from sys import platform
+
+if platform == "linux" or platform == "linux2":
+    os.system("pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu")
+elif platform == "win32" or platform == "darwin":
+    os.system("pip3 install torch torchaudio torchvision")
+
+os.system("pip install -r requirements.txt")
+os.system("python3 app.py")
