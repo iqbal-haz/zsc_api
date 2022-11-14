fc9a60c6e897c8b50fd6de09ec366aea410de1d7
AI Engineer <alif@bahasa.ai>
2022-11-14 22:31:54 +0700

    installing package in app.py, deleted cmd.py

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
 from flask import Flask,request, jsonify
-from importlib.util import find_spec
 from transformers import pipeline
 from sys import platform
-import re
 import os
 
-with open("requirements.txt", 'r') as f:
-    for package in f.readlines():
-        package = package.rstrip()
-        punc = re.search(r'\W+', package)
-        punc = punc.start() if punc else None
-        pkg_name = package[:punc]
+if platform == "linux" or platform == "linux2":
+    os.system("pip3 install -q torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu")
+elif platform == "win32" or platform == "darwin":
+    os.system("pip3 install -q torch torchaudio torchvision")
 
-        if find_spec(pkg_name) is None:
-            os.system(f"pip install {package}")
-
-# if platform == "linux" or platform == "linux2":
-#     os.system("pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu")
-# elif platform == "win32" or platform == "darwin":
-#     os.system("pip3 install torch torchaudio torchvision")
-
-# os.system("pip install -r requirements.txt")
+os.system("pip install -q -r requirements.txt")
 classifier = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
 
 app = Flask(__name__)
