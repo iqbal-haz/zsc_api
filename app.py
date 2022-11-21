b82f10f7c7cc9c8f1b7ed5c0a0784da751f872c8
AI Engineer <alif@bahasa.ai>
2022-11-21 17:29:18 +0700

    revert to original

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 from flask import Flask,request, jsonify
 from transformers import pipeline
-from sys import platform
-import os
-
-if platform == "linux" or platform == "linux2":
-    os.system("pip3 install -q torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu")
-elif platform == "win32" or platform == "darwin":
-    os.system("pip3 install -q torch torchaudio torchvision")
-
-os.system("pip install -q -r requirements.txt")
 classifier = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
 
 app = Flask(__name__)
